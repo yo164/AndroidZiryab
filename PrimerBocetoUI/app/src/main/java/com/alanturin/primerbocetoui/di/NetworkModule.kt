22133c5d90d3bec0cabcb2963d6d87d7c361dc3d
angela1006 <amormat1010@g.educaand.es>
2026-01-28 11:10:29 +0100

    cambios url

--- PrimerBocetoUI/app/src/main/java/com/alanturin/primerbocetoui/di/NetworkModule.kt
+++ PrimerBocetoUI/app/src/main/java/com/alanturin/primerbocetoui/di/NetworkModule.kt
@@ -19,7 +19,7 @@ object NetworkModule {
     @Singleton
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:3000/")
+            .baseUrl("http://localhost:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
